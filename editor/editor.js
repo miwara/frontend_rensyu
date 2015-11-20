cba74189e79e5de2f299d1a68d3807c33763baf6
miuraK <miura@sencorp.co.jp>
2015-11-20 23:38:48 +0900

    足りなかった

--- editor/editor.js
+++ editor/editor.js
@@ -1,10 +1,49 @@
-editor = ace.edit("input_txt");
-editor.gitSession().setMode("ace/mode/javascript");
-editor.setTheme("ace/theme/twilight");
-
+var fs = require('fs');
 var remote = require('remote');
 var dialog = remote.require('dialog');
 var browserWindow = remote.require('browser-window');
+
+var inputArea = null;
+var inputTxt = null;
+var footerArea = null;
+
+var currentPath = "";
+var editor = null;
+
+/**
+ *  Webページ読み込み時の処理
+ */
+function onLoad() {
+    inputArea = document.getElementById("input.area");
+    inputTxt = document.getElementById("input.txt");
+    footerArea = document.getElementById("footer_fixed");
+
+    editor = ace.edit("input_txt");
+    editor.gitSession().setMode("ace/mode/javascript");
+    editor.setTheme("ace/theme/twilight");
+
+    // ドラッグ&ドロップ関連
+    document.ondragover = document.ondrop = function (e) {
+	e.preventDefault();
+	return false;
+    };
+
+    inputArea.ondragover = Function () {
+	return false;
+    };
+
+    inputArea.ondragover = inputArea.ondragover = function () {
+	return false;
+    };
+
+    inputArea.ondrop = function (e) {
+	e.preventDefault();
+	var file = e.dataTransfer.files[0];
+	readFile(file.path);
+	return false;
+    };
+};
+
 /**
  *  読み込みするためのファイルを開く
  */
@@ -27,6 +66,21 @@ function openLoadFile() {
 		readFile(filenames[0]);
 	    }
 	});
+}
+
+/**
+ *  テキストを読み込み，テキストを入力エリアに設定
+ */
+function readFile(path) {
+    currentPath = path;
+    fs.readFile(path, function (error, text) {
+	if (error != null) {
+	    alert('error : ' + error);
+	    return;
+	}
+	footerArea.innerHTML = path;
+	editor.setValue(text.toString(), -1);
+    });
 }
 
 /**
@@ -56,8 +110,6 @@ function saveFile() {
 	});
 }
 
-var fs = require('fs');
-
 /**
  *  ファイルを書き込む
  */
@@ -67,4 +119,30 @@ function writeFile(path, data) {
 	    alert('error : ' + error);
 	}
     });
+}
+
+/**
+ *  新規ファイルを保存
+ */
+function saveNewFile() {
+    var win = browserWindow.getFocusedWindow();
+    dialog.showSaveDialog(
+	win,
+	{
+	    properties : ['openFile'],
+	    filters    : [
+		{
+		    name       : 'Documents',
+		    extensions : ['txt', 'text', 'html', 'js']
+		}
+	    ]
+	},
+	function (fileName) {
+	    if (fileName) {
+		var data = editor.getValue();
+		currentPath = fileName;
+		writeFile(currentPath, data);
+	    }
+	}
+    );
 }
