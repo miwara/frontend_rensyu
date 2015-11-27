71245e9994ac810447fc023a607ed215989f770c
miuraK <miura@sencorp.co.jp>
2015-11-27 15:49:54 +0900

    add comment

--- editor2/editor.js
+++ editor2/editor.js
@@ -1,16 +1,16 @@
-var fs = require('fs');
-var remote = require('remote');
+var fs = require('fs'); /* ファイル操作 https://nodejs.org/api/fs.html */
+var remote = require('remote'); /* http://qiita.com/Misumi_Rize/items/a434cc0d17e5c6b85147 */
 var dialog = remote.require('dialog');
-var browserWindow = remote.require('browser-window');
+var browserWindow = remote.require('browser-window'); /* http://electron.atom.io/docs/v0.35.0/api/browser-window/ */
 var editor = null;
 
 function onLoad() {
-    editor = ace.edit("editor");
-    editor.getSession().setMode('ace/mode/javascript');
+    editor = ace.edit("editor"); /* 入力エリアを指定 */
+    editor.getSession().setMode('ace/mode/javascript'); /* シンタックスのモードを javascript に */
 }
 
 function openLoadFile(file) {
-    dialog.showOpenDialog(
+    dialog.showOpenDialog( /* http://nhamada.hatenablog.com/entry/2015/05/24/210639 */
 	browserWindow.getFocusedWindow(),
 	{
 	    properties: ['openFile'],
@@ -35,6 +35,6 @@ function readFile(path) {
 	    return;
 	}
 
-	editor.setValue(text.toString(), -1);
+	editor.setValue(text.toString(), -1); /* エディタにセット */
     });
 }
