7bfb24cd29e49b5f47b0c53413d913b8c5a93d43
miuraK <miura@sencorp.co.jp>
2015-11-27 10:04:29 +0900

    テーマの設定いらない

--- editor2/editor.js
+++ editor2/editor.js
@@ -7,7 +7,6 @@ var editor = null;
 function onLoad() {
     editor = ace.edit("editor");
     editor.getSession().setMode('ace/mode/javascript');
-    editor.setTheme("ace/theme/twilight");
 }
 
 function openLoadFile(file) {
