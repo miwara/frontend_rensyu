dae7d19b6157a2deb00c4d24d960770f3eb2fbe1
miuraK <miura@sencorp.co.jp>
2015-12-04 20:39:41 +0900

    first commit

---
+++ dragdrop_folder/dragdrop.js
@@ -0,0 +1,29 @@
+function onLoad() {
+    var holder = document.getElementById('holder');
+
+    document.ondragover = document.ondrop = Function (e) {
+	e.preventDefault(); // イベントの伝搬をやめて，アプリケーションのhtmlとファイルが差し替わらないようにする
+	return false;
+    };
+
+    /** holderエリアにドラッグされた場合 */
+    holder.ondragover = function() {
+	return false;
+    };
+
+    /** holderエリアから外れたorドラッグが終了した */
+    holder.ondragleave = holder.ondragend = function () {
+	return false;
+    };
+
+    /** holderエリアにドロップされた */
+    holder.ondrop = function (e) {
+	e.preventDefault();
+
+	var file = e.dataTransfer.files[0];
+	holder.innerText = file.name;
+	console.log(file.path);
+
+	return false;
+    };
+}
