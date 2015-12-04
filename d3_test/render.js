c7117a42a7fe7963d0c76322f2b25376544af767
miuraK <miura@sencorp.co.jp>
2015-12-04 18:08:31 +0900

    ヒストグラムを書いてみた

--- d3_test/render.js
+++ d3_test/render.js
@@ -12,3 +12,30 @@ d3.csv("./data.csv", function(error, list) {
 	    return d["value"];
 	})
 });
+
+
+
+/**
+ *  棒グラフを描く
+ */
+d3.csv('./data.csv', function(csvdata) {
+    var dataset = [];
+    for (var i = 0; i < csvdata.length; i++) {
+	dataset.push(csvdata[i]['value']);
+    };
+    make(dataset);
+});
+
+function make(dataset) {
+    d3.select('svg').selectAll('rect')
+	.data(dataset)
+	.enter()
+	.append('rect')
+	.attr({
+	    x : function(d, i){ return i * 30; },
+	    y : function(d){ return 300 -d; },
+	    width : 15,
+	    height : function(d){ return d; },
+	    fill : '#6fbadd'
+	});
+}
