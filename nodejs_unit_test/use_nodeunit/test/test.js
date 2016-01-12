3184cd4a80dec257a004e7651eeaeeedc0f55273
miuraK <miura@sencorp.co.jp>
2016-01-12 18:59:25 +0900

    Mocha と同じ例の方が良いと思った（非同期関数のテスト方法はまだわからない）

--- nodejs_unit_test/use_nodeunit/test/test.js
+++ nodejs_unit_test/use_nodeunit/test/test.js
@@ -1,6 +1,13 @@
-var calc = require('../calc');
+var index = require('../index');
 
-exports['calcula add'] = function (test) {
-  test.equal(calc.add(1,2), 3);
+exports['greet'] = function(test) {
+  test.equal(index.greet('taro'), 'Hello,taro');
   test.done();
 };
+/*
+exports['greetAsync'] = function(test) {
+  test.expect(2);
+  index.greetAsync('hanako', function (greet) {
+  });
+};
+*/
