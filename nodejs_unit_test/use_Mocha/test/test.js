8458a92fc7a31389c90372978c3bbda45160a51a
miuraK <miura@sencorp.co.jp>
2016-01-12 17:32:55 +0900

    mochaで非同期関数のテスト

--- nodejs_unit_test/use_Mocha/test/test.js
+++ nodejs_unit_test/use_Mocha/test/test.js
@@ -8,3 +8,20 @@ describe('index', function () {
     });
   });
 });
+
+describe('index', function () {
+  describe('greet', function () {
+    it('引数に応じて決まった文字列を返すこと', function () {
+      assert.equal(index.greet('taro'), 'Hello,taro');
+    });
+  });
+
+  describe('greetAsync', function () {
+    it('引数に応じてコールバック内で決まった文字列になること', function (done) {
+      index.greetAsync('hanako', function (greet) {
+        assert.equal(greet, 'Hello,hanako');
+        done();
+      });
+    });
+  });
+});
