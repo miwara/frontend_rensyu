c9ad815a36bed6a316ff8c26b3b9c86757b3e9d5
miuraK <miura@sencorp.co.jp>
2016-01-13 09:53:57 +0900

    モダンに

--- nodejs_unit_test/use_nodeunit/test/test.js
+++ nodejs_unit_test/use_nodeunit/test/test.js
@@ -1,12 +1,12 @@
 var index = require('../index');
 
-exports['greet'] = function(test) {
+exports.greet = function(test) {
   test.equal(index.greet('taro'), 'Hello,taro');
   test.done();
 };
 
 // 非同期関数のテスト（書き方を間違えている気がする）
-exports['greetAsync'] = function(test) {
+exports.greetAsync = function(test) {
   test.expect(2);
   test.equal(index.greet('taro'), 'Hello,taro');
   index.greetAsync('hanako', function (greet) {
