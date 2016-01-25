ad24c7d717ef1c62be74e9bde7144d6ecacb6a5b
miuraK <miura@sencorp.co.jp>
2016-01-25 17:13:22 +0900

    sessionStorage も同様にやってみた

--- web_storage/webstorage.js
+++ web_storage/webstorage.js
@@ -1,11 +1,20 @@
 "use strict";
 window.addEventListener('DOMContentLoaded', () => {
+  // localStorage のテスト
   saveMethodToLocalStorage();
   readMethodToLocalStorage();
   savePropertyToLocalStorage();
   readPropertyToLocalStorage();
   saveHashToLocalStorage();
   readHashToLocalStorage();
+
+  // sessionStorage のテスト
+  saveMethodToSessionStorage();
+  readMethodToSessionStorage();
+  savePropertyToSessionStorage();
+  readPropertyToSessionStorage();
+  saveHashToSessionStorage();
+  readHashToSessionStorage();
 });
 
 // メソッドで localStorage を操作
@@ -33,4 +42,31 @@ let saveHashToLocalStorage = () => {
 
 var readHashToLocalStorage = () => {
   alert(`use hash: read ${localStorage['key3']} from localStorage`);
+};
+
+// メソッドで sessionStorage を操作
+let saveMethodToSessionStorage = () => {
+  sessionStorage.setItem('key1', 'val1');
+};
+
+let readMethodToSessionStorage = () => {
+  alert(`use method: read ${sessionStorage.getItem('key1')} from sessionStorage`);
+};
+
+// プロパティで localstorage を操作
+let savePropertyToSessionStorage = () => {
+  sessionStorage.key2 = 'val2';
+};
+
+let readPropertyToSessionStorage = () => {
+  alert(`use property: read ${sessionStorage.key2} from sessionStorage`);
+};
+
+// 連想配列で sessionStorage を操作
+let saveHashToSessionStorage = () => {
+  sessionStorage['key3'] = 'val3';
+};
+
+var readHashToSessionStorage = () => {
+  alert(`use hash: read ${sessionStorage['key3']} from sessionStorage`);
 };
