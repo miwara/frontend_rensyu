4f7a75c034b5a215f4ad88294f5562e82c90dfda
miuraK <miura@sencorp.co.jp>
2016-01-25 16:45:59 +0900

    メソッドで localStorage に保存する

--- web_storage/webstorage.js
+++ web_storage/webstorage.js
@@ -1,8 +1,17 @@
+"use strict";
 window.addEventListener('DOMContentLoaded', () => {
-  if (!localStorage) {
-    alert("not localStorage");
-  }
-  if (!sessionStorage) {
-    alert("not sessionStorage");
-  }
+  saveMethod();
+  readMethod();
 });
+
+// メソッドで localStorage に保存する
+let saveMethod = () => {
+  let key = 'key1';
+  let val = 'val1';
+  localStorage.setItem(key, val);
+};
+
+let readMethod = () => {
+  let val = localStorage.getItem('key1');
+  alert(val);
+};
