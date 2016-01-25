41baf98f4418ab29e1e8420ebe53fed12f3bf3c4
miuraK <miura@sencorp.co.jp>
2016-01-25 16:50:07 +0900

    プロパティで localStorage に保存する

--- web_storage/webstorage.js
+++ web_storage/webstorage.js
@@ -1,7 +1,7 @@
 "use strict";
 window.addEventListener('DOMContentLoaded', () => {
-  saveMethod();
-  readMethod();
+  saveProperty();
+  readProperty();
 });
 
 // メソッドで localStorage に保存する
@@ -14,4 +14,15 @@ let saveMethod = () => {
 let readMethod = () => {
   let val = localStorage.getItem('key1');
   alert(val);
+};
+
+// プロパティで localstorage に保存する
+let saveProperty = () => {
+  let val = 'val2';
+  localStorage.key2 = val;
+};
+
+let readProperty = () => {
+  let key2 = localStorage.key2;
+  alert(key2);
 };
