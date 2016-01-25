68fc2965c2553c56cc6421e736d0e4da8de93d3c
miuraK <miura@sencorp.co.jp>
2016-01-25 16:32:21 +0900

    window作成

---
+++ web_storage/main.js
@@ -0,0 +1,22 @@
+var app = require('electron').app;
+var BrowserWindow = require('electron').BrowserWindow;
+
+require('crash-reporter').start();
+
+var mainWindow = null;
+
+app.on('window-all-closed', function() {
+    if (process.platform != 'darwin') {
+	app.quit();
+    }
+});
+
+app.on('ready', function(){
+    mainWindow = new BrowserWindow({width: 800, height: 600});
+    mainWindow.loadURL('file://' + __dirname + '/index.html');
+    mainWindow.openDevTools();
+
+    mainWindow.on('closed', function() {
+	mainWindow = null;
+    });
+});
