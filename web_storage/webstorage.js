786754d1b83c6f136db0814e1e5aac92e923e6e3
miuraK <miura@sencorp.co.jp>
2016-01-25 17:03:03 +0900

    整理

--- web_storage/webstorage.js
+++ web_storage/webstorage.js
@@ -1,39 +1,36 @@
 "use strict";
 window.addEventListener('DOMContentLoaded', () => {
-  saveHash();
-  readHash();
+  saveMethodToLocalStorage();
+  readMethodToLocalStorage();
+  savePropertyToLocalStorage();
+  readPropertyToLocalStorage();
+  saveHashToLocalStorage();
+  readHashToLocalStorage();
 });
 
-// メソッドで localStorage に保存する
-let saveMethod = () => {
-  let key = 'key1';
-  let val = 'val1';
-  localStorage.setItem(key, val);
+// メソッドで localStorage を操作
+let saveMethodToLocalStorage = () => {
+  localStorage.setItem('key1', 'val1');
 };
 
-let readMethod = () => {
-  let val = localStorage.getItem('key1');
-  alert(val);
+let readMethodToLocalStorage = () => {
+  alert(`use method: read ${localStorage.getItem('key1')} from localStorage`);
 };
 
-// プロパティで localstorage に保存する
-let saveProperty = () => {
-  let val = 'val2';
-  localStorage.key2 = val;
+// プロパティで localstorage を操作
+let savePropertyToLocalStorage = () => {
+  localStorage.key2 = 'val2';
 };
 
-let readProperty = () => {
-  let key2 = localStorage.key2;
-  alert(key2);
+let readPropertyToLocalStorage = () => {
+  alert(`use property: read ${localStorage.key2} from localStorage`);
 };
 
-// 連想配列で localStorage に保存する
-let saveHash = () => {
-  let val3 = "val3";
-  localStorage['key3'] = val3;
+// 連想配列で localStorage を操作
+let saveHashToLocalStorage = () => {
+  localStorage['key3'] = 'val3';
 };
 
-var readHash = () => {
-  let key3 = localStorage['key3'];
-  alert(key3);
+var readHashToLocalStorage = () => {
+  alert(`use hash: read ${localStorage['key3']} from localStorage`);
 };
