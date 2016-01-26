e293ca7619ff2ae47bc68841f97994314c73617a
miuraK <miura@sencorp.co.jp>
2016-01-26 12:39:49 +0900

    ipc を使って別のwindowを作れたので

--- ipc_test/main.js
+++ ipc_test/main.js
@@ -1,15 +1,14 @@
 "use strict";
 let app = require('electron').app;
 let BrowserWindow = require('electron').BrowserWindow;
+let ipcMain = require('electron').ipcMain;
 
 require('crash-reporter').start();
 
 let mainWindow = null;
 
 app.on('window-all-closed', function() {
-    if (process.platform != 'darwin') {
-	app.quit();
-    }
+  app.quit();
 });
 
 app.on('ready', function(){
@@ -20,4 +19,27 @@ app.on('ready', function(){
     mainWindow.on('closed', function() {
 	mainWindow = null;
     });
+});
+
+// 同期
+ipcMain.on('synchronous-SubWindow', function(event) {
+  let subWindow = new BrowserWindow({width: 200, height:500});
+  subWindow.loadURL('file://' + __dirname + '/synchronousSubWindow.html');
+
+  event.returnValue = 'create sub window(synchronous)';
+
+  subWindow.on('closed', function() {
+    subWindow = null;
+  });
+});
+
+// 非同期
+ipcMain.on('asynchronous-SubWindow', function(event) {
+  let subWindow = new BrowserWindow({width: 200, height:500});
+  subWindow.loadURL('file://' + __dirname + '/asynchronousSubWindow.html');
+  event.sender.send('asynchronous-reply', 'create sub window(asynchronous)');
+
+  subWindow.on('closed', function() {
+    subWindow = null;
+  });
 });
