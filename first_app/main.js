5898fc4ddfb7cde1a3d60c4f6e19efe7f27e10d4
miuraK <miura@sencorp.co.jp>
2016-01-27 09:18:28 +0900

    アロー関数を使う

--- first_app/main.js
+++ first_app/main.js
@@ -6,16 +6,12 @@ require('crash-reporter').start();
 
 let mainWindow = null;
 
-app.on('window-all-closed', function() {
-  app.quit();
-});
+app.on('window-all-closed', () => app.quit());
 
-app.on('ready', function(){
+app.on('ready', () => {
     mainWindow = new BrowserWindow({width: 800, height: 600});
     mainWindow.loadURL('file://' + __dirname + '/index.html');
     mainWindow.openDevTools();
 
-    mainWindow.on('closed', function() {
-	mainWindow = null;
-    });
+    mainWindow.on('closed', () => mainWindow = null);
 });
