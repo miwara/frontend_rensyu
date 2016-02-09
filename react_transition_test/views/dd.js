9b843e9cf92a13838565082526c931266f620f27
miuraK <miura@sencorp.co.jp>
2016-02-09 16:52:11 +0900

    ddできるように

--- react_transition_test/views/dd.js
+++ react_transition_test/views/dd.js
@@ -21,27 +21,15 @@ let Holder = React.createClass ({
     e.preventDefault();
     sessionStorage.setItem('path', e.dataTransfer.files[0].path);
     sessionStorage.setItem('name', e.dataTransfer.files[0].name);
+    this.history.pushState(null, '/login');
     return false;
   },
 
-  handleSubmit: function(e) {
-    e.preventDefault();
-    this.history.pushState(null, '/login');
-  },
-
   render: function() {
-    let style = {
-      padding: '100px',
-      borderColor: '#000',
-      borderStyle: 'solid'
-    };
     return (
-      //        <div style={style} onDragover={this.dragover} onDragLeave={this.dragleave} onDragEnd={this.dragend} onDrop={this.drop}></div>
         <div>
         <h1>D&D</h1>
-        <form onSubmit={this.handleSubmit}>
-        <button type="submit">次へ</button>
-        </form>
+        <div className="holder" onDragover={this.dragover} onDragLeave={this.dragleave} onDragEnd={this.dragend} onDrop={this.drop}></div>
         </div>
     );
   }
