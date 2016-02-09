6acc9e5d6b4e0d1a6d14bf24c5a651ff1b24d709
miuraK <miura@sencorp.co.jp>
2016-02-09 11:38:58 +0900

    画面遷移ができた

--- react_and_scss_test/entry.js
+++ react_and_scss_test/entry.js
@@ -1,16 +1,47 @@
 "use strict";
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import { Router, Route, hashHistory } from 'react-router';
+import { Router, Route, IndexRoute, Link } from 'react-router';
 
 class App extends Component {
   render() {
-    return <h1>Hello form React!</h1>;
+    return (
+        <div>
+        <ul>
+        <li><Link to="/">index</Link></li>
+        <li><Link to="/a">page A</Link></li>
+        <li><Link to="/b">page B</Link></li>
+        </ul>
+        <div>{this.props.children}</div>
+        </div>
+    );
+  }
+}
+
+class Index extends Component {
+  render() {
+    return <div>Index</div>;
+  }
+}
+
+class PageA extends Component {
+  render() {
+    return <div>A</div>;
+  }
+}
+
+class PageB extends Component {
+  render() {
+    return <div>B</div>;
   }
 }
 
 render((
-    <Router history={hashHistory}>
-    <Route path="/" component={App} />
+    <Router>
+      <Route path="/" component={App}>
+        <IndexRoute component={Index} />
+        <Route path="/a" component={PageA} />
+        <Route path="/b" component={PageB} />
+      </Route>
     </Router>
 ), document.querySelector("#reactroot"));
