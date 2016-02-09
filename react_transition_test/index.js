8f458141811e667f68012d3872655c2720bc4278
miuraK <miura@sencorp.co.jp>
2016-02-09 14:53:10 +0900

    mixin が使いたいので古い書き方に戻す

--- react_transition_test/index.js
+++ react_transition_test/index.js
@@ -3,27 +3,27 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import { Router, Route} from 'react-router';
 
-class Holder extends Component {
-  dragover() {
+let Holder = React.createClass ({
+  dragover: function() {
     return false;
-  }
+  },
 
-  dragleave() {
+  dragleave: function() {
     return false;
-  }
+  },
 
-  dragend() {
+  dragend: function() {
     return false;
-  }
+  },
 
-  drop(e) {
+  drop: function(e) {
     e.preventDefault();
     sessionStorage.setItem('path', e.dataTransfer.files[0].path);
     sessionStorage.setItem('name', e.dataTransfer.files[0].name);
     return false;
-  }
+  },
 
-  render() {
+  render: function() {
     let style = {
       padding: '100px',
       borderColor: '#000',
@@ -33,7 +33,7 @@ class Holder extends Component {
         <div style={style} onDragover={this.dragover} onDragLeave={this.dragleave} onDragEnd={this.dragend} onDrop={this.drop}></div>
     );
   }
-}
+});
 
 render((
     <Router>
