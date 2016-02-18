052bd664b61d0dc18a70a561dc5f746907c35f64
miuraK <miura@sencorp.co.jp>
2016-02-18 13:13:17 +0900

    webpack の設定ファイル

---
+++ webpack_test/webpack.config.js
@@ -0,0 +1,21 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: './src/build',
+    filename: 'app.js'
+  },
+  module: {
+    loaders: [{
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_moduless/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+    }]
+  },
+  target: 'node'
+};
