56797f1e44aa40be3759863eba83f2ef1f56763b
miuraK <miura@sencorp.co.jp>
2016-10-24 15:19:28 +0900

    fix indent

--- flux/js/components/MainSection.react.js
+++ flux/js/components/MainSection.react.js
@@ -29,14 +29,14 @@ let MainSection = React.createClass({
 
     return (
       <section id="main">
-      <input
-        id="toggle-all"
-        type="checkbox"
-        onChange={this._onToggleCompleteAll}
-        checked={this.props.areAllComplete ? 'checked' : ''}
-      />
-      <label htmlFor="toggle-all">Mark all as complete</label>
-      <ul id="todo-list">{todos}</ul>
+       <input
+         id="toggle-all"
+         type="checkbox"
+         onChange={this._onToggleCompleteAll}
+         checked={this.props.areAllComplete ? 'checked' : ''}
+       />
+       <label htmlFor="toggle-all">Mark all as complete</label>
+       <ul id="todo-list">{todos}</ul>
       </section>
     );
   }
