24a19d4597a2179627ed170f85a80d045b4ba1a9
miuraK <miura@sencorp.co.jp>
2016-10-24 15:42:37 +0900

    TodoTextInput作成

---
+++ flux/js/components/TodoTextInput.react.js
@@ -0,0 +1,50 @@
+'use strict';
+import React from 'react';
+
+let ENTER_KEY_CODE = 13;
+
+let TodoTextInput = React.createClass({
+  propsTypes: {
+    className: React.PropTypes.string,
+    id: React.PropTypes.string,
+    placeholder: React.PropTypes.string,
+    onSave: React.PropTypes.func.isRequired,
+    value: React.PropTypes.string
+  },
+
+  getInitialState: function() {
+    return {
+      value: this.props.value || ''
+    };
+  },
+
+  _save: function(event) {
+    this.props.onSave(this.sate.value);
+    this.setState({
+      value: event.target.value
+    });
+  },
+
+  _onKeyDown: function(event) {
+    if (event.keyCode === ENTER_KEY_CODE) {
+      this._save();
+    }
+  },
+
+  render: function() {
+    return (
+     <input
+       className={this.props.className}
+       id={this.props.id}
+       placeholder={this.props.placeholder}
+       onBlur = {this._save}
+       onChange = {this._onChange}
+       onKeyDown = {this._onKeyDown}
+       value={this.state.value}
+       autoFocus={true}
+     />
+    );
+  }
+});
+
+module.exports = TodoTextInput;
