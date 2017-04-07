47c4c2d09a014a19ccaafad7b597ff1dc0f3856a
miwara <aruim.jon@gmail.com>
2017-04-07 21:37:55 +0900

    ユーザ入力の制御はこうする

--- vuejs/js/app.js
+++ vuejs/js/app.js
@@ -31,3 +31,15 @@ let app4 = new Vue({
     ]
   }
 });
+
+let app5 = new Vue({
+  el: '#app-5',
+  data: {
+    message: 'Hello Vue.js',
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split('').reverse().join('')
+    }
+  }
+});
